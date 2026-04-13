20e954c2bc16185c9ff4a0a5c4d68a20fe6e5c95
nalindu <nalindugra@gmail.com>
2026-04-13 21:07:23 +0530

    Created: react project, backend project Added: TailwindCSS, ShadCN, dependencies

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+        <h1 class="text-3xl font-bold underline">
+            Hello world!
+        </h1>
+        <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
